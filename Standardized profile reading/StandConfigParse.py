96c39d8c5dabb98e6f0b996c8cbc957ca500fae1
VirtualNoman <145430853+VirtualNoman@users.noreply.github.com>
2023-09-19 15:37:11 +0800

    Add files via upload

--- Standardized profile reading/StandConfigParse.py
+++ Standardized profile reading/StandConfigParse.py
@@ -2,25 +2,25 @@ import configparser
 
 
 class ConfigRead:
-    """读取和管理配置文件的类。配置数据结构:
+    """Classes that read and manage configuration files. Configure data structure:
 
     [section]
     option=value
     option=value
 
     Attributes:
-        file_path (str): 配置文件的路径。
-        conf (configparser.ConfigParser): ConfigParser对象，用于读取和管理配置文件内容。
+        file_path (str): The path to the configuration file.
+        conf (configparser.ConfigParser): ConfigParserObject to read and manage configuration file contents.
     Note:
         Remember to use the save_config method to save to a file after modifying the instance
     """
 
     def __init__(self, configfile_path: str):
         """
-        初始化 ConfigRead 类。
+        Initialize the ConfigRead class.
 
         Args:
-            configfile_path (str): 配置文件的路径。
+            configfile_path (str): The path to the configuration file.
         """
         self.file_path = configfile_path
         self.conf = configparser.ConfigParser()
@@ -28,117 +28,117 @@ class ConfigRead:
 
     def read_sections(self) -> list[str]:
         """
-        获取配置文件中的所有 sections。
+        Gets all sections in the configuration file.
 
         Returns:
-            list[str]: 配置文件中的所有 sections 的列表。
+            list[str]: A list of all sections in the configuration file.
         """
         return self.conf.sections()
 
     def section_exist(self, section_name) -> bool:
         """
-        检查指定的 section 是否存在。
+        Checks whether the specified section exists.
 
         Args:
-            section_name: 要检查的 section 的名称。
+            section_name: The name of the section to check.
 
         Returns:
-            bool: 如果 section 存在则返回 True，否则返回 False。
+            bool: True if the section exists, False otherwise.
         """
         return self.conf.has_section(section_name)
 
     def read_options(self, section) -> list[str]:
         """
-        获取指定 section 下的所有 options，即 keys。
+        Get all options (keys) in the specified section.
 
         Args:
-            section: 要获取 options 的 section 名称。
+            section: The name of the section to get options from.
 
         Returns:
-            list[str]: 指定 section 下的所有 options 的列表。
+            list[str]: The list of all options in the specified section.
         """
         return self.conf.options(section)
 
     def option_exist(self, section, option) -> bool:
         """
-        检查指定 section 下的 option 是否存在。
+        Check if the specified option exists in the specified section.
 
         Args:
-            section: option 所属的 section 名称。
-            option: 要检查的 option 名称。
+            section: The name of the section where the option belongs.
+            option: The name of the option to check.
 
         Returns:
-            bool: 如果 option 存在则返回 True，否则返回 False。
+            bool: True if the option exists, False otherwise.
         """
         return self.conf.has_option(section, option)
 
     def get_items(self, section) -> list[tuple]:
         """
-        获取指定 section 下的所有键值对。
+        Get all key-value pairs in the specified section.
 
         Args:
-            section: 要获取 items 的 section 名称。
+            section: The name of the section to get items from.
 
         Returns:
-            list[tuple]: 指定 section 下的所有键值对的列表。
+            list[tuple]: The list of all key-value pairs in the specified section.
         """
         return self.conf.items(section)
 
     def read_value(self, section, option) -> str:
         """
-        获取指定 section 下 option 键对应的值。
+        Get the value of the specified option in the specified section.
 
         Args:
-            section: option 所属的 section 名称。
-            option: 要获取 value 的 option 名称。
+            section: The name of the section where the option belongs.
+            option: The name of the option to get the value of.
 
         Returns:
-            str: option 键对应的值。
+            str: The value of the option.
         """
         return self.conf.get(section, option)
 
     def remove_section(self, section):
         """
-        删除指定的 section。
+        Remove the specified section.
 
         Args:
-            section: 要删除的 section 名称。
+            section: The name of the section to remove.
         """
         self.conf.remove_section(section)
 
     def remove_item(self, section, option):
         """
-        删除指定 section 下的键值对。
+        Remove the specified key-value pair in the specified section.
 
         Args:
-            section: 要删除键值对的 section 名称。
-            option: 要删除的 option 名称。
+            section: The name of the section where the key-value pair belongs.
+            option: The name of the option to remove.
         """
         self.conf.remove_option(section, option)
 
     def add_section(self, section_name):
         """
-        添加新的 section。
+        Add a new section.
 
         Args:
-            section_name: 要添加的 section 名称。
+            section_name: The name of the section to add.
         """
         self.conf.add_section(section_name)
 
     def add_item(self, section, option, value):
         """
-        添加新的键值对。
+        Add a new key-value pair.
 
         Args:
-            section: 要添加键值对的 section 名称。
-            option: 要添加的 option 名称。
-            value: option 对应的值。
+            section: The name of the section where the key-value pair belongs.
+            option: The name of the option to add.
+            value: The value of the option.
         """
         self.conf.set(section, option, value)
 
     def save_config(self):
         """
-        保存配置文件更改到文件中，并使其生效。
+        Save the configuration changes to the file and make them effective.
         """
         with open(self.file_path, "w+") as f:
             self.conf.write(f)
